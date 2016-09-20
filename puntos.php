54c5f38c86e79a28b00a2907dcf6811cd513ed7d
yanethcast <nycastro99@misena.edu.co>
2016-09-20 01:06:33 -0500

    archivos de geolocalizacion
    archivos para ingresar veterinarias y al ingresar se crea las coordenadas de localización en el mapa de cada una

---
+++ puntos.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'class/conexion.php';//INCLUIR CONEXION DE BASE DE DATOS
+
+class Puntos
+{
+    private $r;
+    public function __construct()
+    {
+        $this->r = array();
+    }
+    
+    public function listar_todo()
+    {
+        $sql= "select * from veterinaria";
+        $con=new Conexion();
+		$db = $con->conectar();
+        $rpta = mysqli_query($db, $sql) or die ("no se encuentran los datos".mysqli_error($db));
+        
+		
+        while($fila = mysqli_fetch_assoc($rpta))
+        {
+            $this->r[] = $fila;
+        }	
+		
+        return $this->r;   
+	}
+	
+	
+}
+  
+		
+?>
